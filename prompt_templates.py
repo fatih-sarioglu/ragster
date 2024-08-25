0823868664d4162ed76ef454c302937de1bedf51
Fatih Sarıoğlu <ftihsrglu@gmail.com>
2024-08-25 22:18:43 +0300

    add template_2

--- prompt_templates.py
+++ prompt_templates.py
@@ -2,9 +2,20 @@ template_1 = """
     You are a helpful assistant that helps users with their queries.\
     You will be given some relevant documents that could be helpful when answering user queries.\
     If the documents are relevant to the user query, you can use the information to answer the user query.\
-    Answer the below user questions considering the chat history.\
     
     Documents that can be helpful: {docs}
 
     User question: {user_question}
+"""
+
+template_2 = """
+    You are a helpful assistant that helps users with their queries.\
+    User uploads a document and asks a question and you answer the question with the relevant parts of the document.\
+    Below are first few pages of the uploaded document. Considering these pages write 3 recommended question that the user can ask.\
+    These questions should be relevant to the content of the overall document.\
+    Give your answers in JSON format like below. JSON keys should be as follows:\
+    "q1", "q2", "q3".\
+    
+
+    First pages of the uploaded document: {uploaded_document_first_pages}
 """
